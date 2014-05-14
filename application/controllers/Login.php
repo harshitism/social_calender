494c7a76c998f5bc85352c8732f512de3008a1f0
sumitawasthi9 <sumit.awasthi027@gmail.com>
2014-05-14 23:47:04 +0530

    l

---
+++ application/controllers/Login.php
@@ -0,0 +1,33 @@
+<?php
+class Login extends CI_Controller{
+function index()
+	{	
+		$this->load->helper(array('form', 'url'));
+		
+		$this->load->library('form_validation');
+		
+		$parameter = array(
+		'user_name' => $this->input->post('user_name'),
+		'password'=> $this->input->post('password')
+		);
+		
+		$this->form_validation->set_rules('user_name', 'User name', 'required|min_length[5]|max_length[12]');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[5]|max_length[20]');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('loginpage');
+		}
+		else
+		{
+			echo 'form submitted';
+			$this->load->model('md_login','',$parameter);
+		}
+	}
+}
+?>
+
+
+
+
+
---
+++ application/controllers/Register.php
@@ -0,0 +1,44 @@
+<?php
+class Register extends CI_Controller{
+function index()
+	{
+		$this->load->helper(array('form', 'url'));
+		
+		$this->load->library('form_validation');
+		
+		$parameter = array(
+        'first_name' => $this->input->post('first_name'),
+		'middle_name' => $this->input->post('middle_name'),
+		'last_name' => $this->input->post('last_name'),
+		'email' => $this->input->post('email'),
+		'dob' => $this->input->post('dob'),
+		'contact' => $this->input->post('contact'),
+        'password' =>	$this->input->post('password'),
+		'gender' => $this->input->post('gender')
+		);
+
+		$this->form_validation->set_rules('first_name', 'First Name', 'required|min_length[5]|max_length[12]|alpha_numeric');
+		$this->form_validation->set_rules('middle_name', 'Middle Name', 'required|min_length[5]|max_length[20]');
+		$this->form_validation->set_rules('last_name', 'Last Name', 'required|min_length[5]|max_length[20]');
+		$this->form_validation->set_rules('email', 'E-Mail', 'required|valid_email');
+		$this->form_validation->set_rules('dob', 'Date of Birth', 'required|valid_date[d/m/y,/]');
+		$this->form_validation->set_rules('contact', 'Contact', 'required|min_length[10]|max_length[10]|numeric');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[5]|max_length[20]|');
+		$this->form_validation->set_rules('gender', 'Gender', 'required|min_length[4]|max_length[10]|!numeric');
+		
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('register');
+		}
+		else
+		{	
+			$this->load->model('md_login','',$parameter);
+		}
+	}
+}
+?>
+
+
+
+
+
