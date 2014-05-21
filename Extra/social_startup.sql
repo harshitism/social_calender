5ee6424ea5fb99ebbf044b55ea80d1acbe466f9b
Ravi <Ravisoni1776@gmail.com>
2014-05-21 09:20:39 +0530

    models added and database updated

--- Extra/social_startup.sql
+++ Extra/social_startup.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 15, 2014 at 05:10 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Generation Time: May 21, 2014 at 02:33 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,6 +19,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `social_startup`
 --
+CREATE DATABASE IF NOT EXISTS `social_startup` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `social_startup`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_details`
+--
+
+CREATE TABLE IF NOT EXISTS `comment_details` (
+  `comment_id` int(100) NOT NULL,
+  `comment` varchar(1000) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_user`
+--
+
+CREATE TABLE IF NOT EXISTS `comment_user` (
+  `comment_id` int(100) NOT NULL AUTO_INCREMENT,
+  `user_id` int(100) NOT NULL,
+  `event_id` int(100) NOT NULL,
+  `date_time` varchar(100) NOT NULL,
+  PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 -- --------------------------------------------------------
 
@@ -69,8 +97,18 @@ CREATE TABLE IF NOT EXISTS `event_photo` (
 
 CREATE TABLE IF NOT EXISTS `follower` (
   `user_id` int(100) NOT NULL,
-  `follower_id` int(100) NOT NULL,
-  `following_type` int(10) NOT NULL DEFAULT '0'
+  `follower_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `following`
+--
+
+CREATE TABLE IF NOT EXISTS `following` (
+  `user_id` int(100) NOT NULL,
+  `following_id` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -101,7 +139,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `contact` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`user_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
