853235c7eddb5b31da0f4a2e911f1819980d8c57
Annu10 <annusoni001@gmail.com>
2014-05-23 09:39:58 +0530

    Create social_startup(1).sql
    database structure

---
+++ application/models/social_startup(1).sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 21, 2014 at 09:25 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `social_startup`
+--
+CREATE DATABASE IF NOT EXISTS `social_startup` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `social_startup`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE IF NOT EXISTS `event` (
+  `event_id` int(100) NOT NULL AUTO_INCREMENT,
+  `user_id` int(100) NOT NULL,
+  `start_date` varchar(50) NOT NULL,
+  `end_date` varchar(50) NOT NULL,
+  PRIMARY KEY (`event_id`),
+  UNIQUE KEY `event_id` (`event_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `event`
+--
+
+INSERT INTO `event` (`event_id`, `user_id`, `start_date`, `end_date`) VALUES
+(1, 1, '5/5/1995', '10/5/1995');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_follower`
+--
+
+CREATE TABLE IF NOT EXISTS `event_follower` (
+  `event_id` int(100) NOT NULL,
+  `follower_id` int(100) NOT NULL,
+  `date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_photo`
+--
+
+CREATE TABLE IF NOT EXISTS `event_photo` (
+  `photo_id` int(100) NOT NULL AUTO_INCREMENT,
+  `event_id` int(100) NOT NULL,
+  `date_added` varchar(100) NOT NULL,
+  `added_by` int(100) NOT NULL,
+  `path` varchar(200) NOT NULL,
+  PRIMARY KEY (`photo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follower`
+--
+
+CREATE TABLE IF NOT EXISTS `follower` (
+  `user_id` int(100) NOT NULL,
+  `follower_id` int(100) NOT NULL,
+  `following_type` int(10) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend`
+--
+
+CREATE TABLE IF NOT EXISTS `friend` (
+  `user_id` int(100) NOT NULL,
+  `friend_id` int(100) NOT NULL,
+  `friendship` int(10) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `friend`
+--
+
+INSERT INTO `friend` (`user_id`, `friend_id`, `friendship`) VALUES
+(45, 26, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `how_to_reach`
+--
+
+CREATE TABLE IF NOT EXISTS `how_to_reach` (
+  `event_id` int(100) NOT NULL,
+  `latitude` varchar(100) NOT NULL,
+  `longitude` varchar(100) NOT NULL,
+  `event_date` varchar(100) NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invite`
+--
+
+CREATE TABLE IF NOT EXISTS `invite` (
+  `invited_friend` text NOT NULL,
+  `friend_id` int(100) NOT NULL,
+  `event_id` int(100) NOT NULL,
+  `location` text NOT NULL,
+  `invited_by` varchar(100) NOT NULL,
+  PRIMARY KEY (`friend_id`),
+  UNIQUE KEY `event_id` (`event_id`),
+  UNIQUE KEY `friend_id` (`friend_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photo`
+--
+
+CREATE TABLE IF NOT EXISTS `photo` (
+  `photo_id` int(100) NOT NULL AUTO_INCREMENT,
+  `event_id` int(100) NOT NULL,
+  `added_by` int(100) NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `date_added` varchar(100) NOT NULL,
+  PRIMARY KEY (`photo_id`),
+  UNIQUE KEY `photo_id` (`photo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(100) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `dob` varchar(50) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `video_id` int(100) NOT NULL AUTO_INCREMENT,
+  `event_id` int(10) NOT NULL,
+  `added_by` text NOT NULL,
+  `path` text NOT NULL,
+  `date_added` varchar(100) NOT NULL,
+  PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
