27b2c5c37be465ae84b8f5e34f3bc232649a51e6
Annu10 <annusoni001@gmail.com>
2014-05-25 12:21:13 +0530

    Update md_invite.php
    model for invite table queries

--- application/models/md_invite.php
+++ application/models/md_invite.php
@@ -5,24 +5,26 @@ class Md_invite extends CI_Model{
 	}
 	
 	/*
-	function insert_invite gets parameters as a string separated by , as inputs 
+	function insert_invite gets a parameter array  as inputs 
 	assuming that the input is in order invited_friend,friend_id,event_id,location,invited_by 
+	it inserts these values in invite table 
 	*/
-	public function insert_invite($params){
+	public function insert_invite($parameter){
 		$this->load->database();
 		$this->load->library('table');
-		$data=explode(",",$params);
+		$data=array(
+		'invited_friend'=>$parameter[0],
+		'friend_id'=>$parameter[1],
+		'event_id'=>$parameter[2],
+		'location'=>$parameter[3],
+		'invited_by'=>$parameter[4]
+		);
 		
-		//check if the same friend has been invited in the same event 
-		$check=$this->db->query("SELECT * FROM invite WHERE friend_id='".$data[1]."' AND event_id='".$data[2]."' ");
-		if(check->num_rows()==0){
-		$query=$this->db->query("INSERT INTO invite (invited_friend,friend_id,event_id,location,invited_by) VALUES('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."')");
+		
+		$this->db->insert('invite',$data);
 		if($this->db->affected_rows() ==1){
+		//$this->load->view('tsucc');
 		return 1;
-		}
-		else{
-		return 0;
-		}
 		}
 		else{
 		return 0;
@@ -30,17 +32,25 @@ class Md_invite extends CI_Model{
 		
 	}
 	/*
-	function edit_invite edits or updates the invite table
-	function edit_invite gets parameters as a string separated by , as inputs 
+	function edit_invite gets a parameter array  as inputs 
 	assuming that the input is in order invited_friend,friend_id,event_id,location,invited_by 
+	it edits or updates these values in invite table 
 	*/
-	public function edit_invite($params){
+	public function edit_invite($parameter){
 		$this->load->database();
 		$this->load->library('table');
-		$data=explode(",",$params);
-
-		$query=$this->db->query("UPDATE invite SET invited_friend='".$data[0]."' AND friend_id='".$data[1]."' AND event_id='".$data[2]."' AND location='".$data[3]."' AND invited_by='".$data[4]."' WHERE event_id='".$data[2]."' AND friend_id='".$data[1]."' ;");
+			$data=array(
+		'invited_friend'=>$parameter[0],
+		'friend_id'=>$parameter[1],
+		'event_id'=>$parameter[2],
+		'location'=>$parameter[3],
+		'invited_by'=>$parameter[4]
+		);
+		$this->db->where('friend_id',$parameter[1]);
+		$this->db->where('event_id',$parameter[2]);
+		$this->db->update('invite',$data);
 		if($this->db->affected_rows() ==1){
+		//$this->load->view('tupsucc');
 		return 1;
 		}
 		else{
@@ -52,15 +62,29 @@ class Md_invite extends CI_Model{
 		function delete_invite takes event_id and friend_id as argument
 		and deletes the row with that event_id and friend_id in table invite 
 	*/
-	public function delete_invite($event_id,$friend_id) {
+	public function delete_invite($friend_id,$event_id) {
 		$this->load->database();
 		$this->load->library('table');
-		$query = $this->db->query("DELETE FROM invite WHERE event_id='".$event_id."' AND friend_id='".$friend_id."';");
+		$this->db->where('friend_id',$friend_id);
+		$this->db->where('event_id',$event_id);
+		$this->db->delete('invite');
 		if($this->db->affected_rows() ==1){
+			//$this->load->view('twsucc');
 			return 1;
 		}
 		else{
 		return 0;
 		}
 	}
+	/*function show_invite displays the contents of invite table in
+		a tabular form
+		*/
+	public function show_invite(){
+
+    $this->load->database();
+    $this->load->library('table');
+	$query = $this->db->query("SELECT * FROM invite");
+	echo $this->table->generate($query); 
+}	
 }
+?>
