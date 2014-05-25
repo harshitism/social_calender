0be59841d826456492d169e076f26590e8932234
Annu10 <annusoni001@gmail.com>
2014-05-25 12:09:21 +0530

    Create photo.php
    inserts,edits,deletes and displays the photo table

---
+++ application/controllers/photo.php
@@ -0,0 +1,63 @@
+<?php
+class Photo extends CI_Controller{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		/*function insert gets parameters separated by / as inputs in the url 
+		assuming the inputs are in order event,added_by,path
+		and photo_id and date_added is assigned automatically
+		it inserts these values in photo table
+		*/
+		public function insert($event,$added_by,$path){
+			$this->load->database();
+				$this->load->helper(array('form', 'url'));
+
+				$this->load->library('form_validation');	
+				
+				$parameter=array($event,$added_by,$path);
+				
+				$this->load->model('Md_photo');
+				$this->Md_photo->insert_photo($parameter);		
+		}
+		/*function edit gets parameters separated by / as inputs in the url 
+		assuming the inputs are in order photo_id,event,added_by,path
+		and date_added is assigned automatically
+		it edits or updates these values in photo table
+		*/
+		public function edit($photo_id,$event_id,$added_by,$path){
+		$this->load->database();
+			$this->load->helper(array('form', 'url'));
+
+			$this->load->library('form_validation');
+			$parameter=array($photo_id,$event_id,$added_by,$path);
+			$this->load->model('Md_photo');
+			$this->Md_photo->edit_photo($parameter);
+		}
+		
+		/*function delete gets photo_id  as parameter  in the url 
+		it deletes the rows from having photo_id=$photo_id 
+		*/
+		public function delete($photo_id){
+			$this->load->database();
+			$this->load->helper(array('form', 'url'));
+
+			$this->load->library('form_validation');	
+			$this->load->model('Md_photo');
+			$this->Md_photo->delete_photo($photo_id);
+		
+		}
+		
+		/*function show displays the contents of invite table in
+		a tabular form
+		*/
+		public function show(){
+		$this->load->database();
+			$this->load->helper(array('form', 'url'));
+
+			$this->load->library('form_validation');	
+			$this->load->model('Md_photo');
+			$this->Md_photo->show_photo_table();
+		}
+}
+?>
