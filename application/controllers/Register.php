5645499fd5912df7e60158a458b689a5c99fd0d1
vishal hanjagi <vishalhanjagi@gmail.com>
2014-05-26 15:04:24 -0700

    sumit: verification model added for email verification and md_register and md_login modified

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -18,22 +18,32 @@ class Register extends CI_Controller{
 		'gender' => $this->input->post('gender')
 		);
 
-		$this->form_validation->set_rules('first_name', 'First Name', 'required|min_length[5]|max_length[12]|alpha_numeric');
-		$this->form_validation->set_rules('middle_name', 'Middle Name', 'required|min_length[5]|max_length[20]');
-		$this->form_validation->set_rules('last_name', 'Last Name', 'required|min_length[5]|max_length[20]');
+		$this->form_validation->set_rules('first_name', 'First Name', 'required|min_length[1]|max_length[12]|alpha_numeric');
+		$this->form_validation->set_rules('middle_name', 'Middle Name', 'required|max_length[20]');
+		$this->form_validation->set_rules('last_name', 'Last Name', 'required|max_length[20]');
 		$this->form_validation->set_rules('email', 'E-Mail', 'required|valid_email');
-		$this->form_validation->set_rules('dob', 'Date of Birth', 'required|valid_date[d/m/y,/]');
+		$this->form_validation->set_rules('dob', 'Date of Birth', 'required|valid_date[dd-mm-yyyy,/]');
 		$this->form_validation->set_rules('contact', 'Contact', 'required|min_length[10]|max_length[10]|numeric');
 		$this->form_validation->set_rules('password', 'Password', 'required|min_length[5]|max_length[20]|');
-		$this->form_validation->set_rules('gender', 'Gender', 'required|min_length[4]|max_length[10]|!numeric');
 		
 		if ($this->form_validation->run() == FALSE)
 		{
 			$this->load->view('register');
 		}
 		else
-		{	
-			$this->load->model('md_login','',$parameter);
+		{
+			$param = implode(",",$parameter);
+			$this->load->model('Md_register');	
+			if($this->Md_register->md_register($param))
+			{
+				$data['succ'] = "successfully registered";
+				$this->load->view('done1',$data);
+			}
+			else
+			{
+				$data['succ'] = "error";
+				$this->load->view('done1',$data);
+			}
 		}
 	}
 }
---
+++ application/controllers/verification.php
@@ -0,0 +1,26 @@
+<?php 
+class Verification extends CI_Controller {
+
+
+	public function __construct()
+		{
+			parent::__construct();
+		}
+		
+	function index($hash)
+	{
+		$this->load->database();
+		//echo "hello";
+		$exist = $this->db->query("SELECT * FROM user WHERE hash = '".$hash."'");
+		if($exist->num_rows()==0)
+		{
+			echo "invalid code";
+			//$this->load->view('done1');
+			return 0;
+		}
+		$this->db->query("UPDATE user SET verify = '1/1/1' where hash = '".$hash."'");
+		$this->load->view('done1');
+	}
+}
+		
+?>
