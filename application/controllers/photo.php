4a1448b87cb7baacdaa9e10b283f73280aa59806
Annu10 <annusoni001@gmail.com>
2014-05-30 10:07:56 +0530

    Update photo.php
    profile integration (some part)

--- application/controllers/photo.php
+++ application/controllers/photo.php
@@ -9,30 +9,34 @@ class Photo extends CI_Controller{
 		and photo_id and date_added is assigned automatically
 		it inserts these values in photo table
 		*/
-		public function insert($event,$added_by,$path){
+		public function insert($event,$addedby,$pathh){
 			$this->load->database();
 				$this->load->helper(array('form', 'url'));
 
 				$this->load->library('form_validation');	
 				
-				$parameter=array($event,$added_by,$path);
+				$parameter=array($event,$addedby,$pathh);
+				
 				
 				$this->load->model('Md_photo');
-				$this->Md_photo->insert_photo($parameter);		
+				$this->Md_photo->insert_photo($parameter);
+				
+				
 		}
 		/*function edit gets parameters separated by / as inputs in the url 
 		assuming the inputs are in order photo_id,event,added_by,path
 		and date_added is assigned automatically
 		it edits or updates these values in photo table
 		*/
-		public function edit($photo_id,$event_id,$added_by,$path){
+		public function edit($photo_id,$event_id,$added_by,$pt){
 		$this->load->database();
 			$this->load->helper(array('form', 'url'));
 
 			$this->load->library('form_validation');
-			$parameter=array($photo_id,$event_id,$added_by,$path);
+			$parameter=array($photo_id,$event_id,$added_by,$pt);
 			$this->load->model('Md_photo');
 			$this->Md_photo->edit_photo($parameter);
+			
 		}
 		
 		/*function delete gets photo_id  as parameter  in the url 
@@ -45,7 +49,7 @@ class Photo extends CI_Controller{
 			$this->load->library('form_validation');	
 			$this->load->model('Md_photo');
 			$this->Md_photo->delete_photo($photo_id);
-		
+			
 		}
 		
 		/*function show displays the contents of invite table in
@@ -56,8 +60,10 @@ class Photo extends CI_Controller{
 			$this->load->helper(array('form', 'url'));
 
 			$this->load->library('form_validation');	
+			echo "<br><br><br>";
 			$this->load->model('Md_photo');
 			$this->Md_photo->show_photo_table();
+			$this->load->view('profile');
 		}
 }
 ?>
