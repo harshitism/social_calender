348ed628f1c4f47bac58e52ef1d293ae79b34173
Harshit <harshit.shift@gmail.com>
2014-06-06 17:28:26 +0530

    Some Corrections

--- application/controllers/Profile_currentuser.php
+++ application/controllers/Profile_currentuser.php
@@ -1,6 +1,7 @@
 <?php
 class Profile_currentuser extends CI_Controller {
-	public function index() {
+	public function index() 
+	{
 		$this->load->library('session');
 		$user_id = $this->session->userdata('user_id');
 		$data['user_id'] = $user_id;
@@ -8,9 +9,9 @@ class Profile_currentuser extends CI_Controller {
 		$data['name'] = $this->Md_profile->get_name($user_id);
 		$data['profilepic'] = $this->Md_profile->get_profilepic($user_id);
 		$this->load->model('Md_event');
-		$even = $this->Md_event->get_event($user_id);
+		//$even = $this->Md_event->get_event($user_id);
 		//$even = array(array("sem prep","fghgh"),array("sem exam","jgffjgjh"),array("sem res","gugugu"));
-		$data['events'] = $even;
+		//$data['events'] = $even;
 		$this->load->model('Md_following');
 		$fol = $this->Md_following->extract_following($user_id);
 		$data['followings'] = count($fol);
@@ -20,7 +21,7 @@ class Profile_currentuser extends CI_Controller {
 		$this->load->model('Md_friend');
 		$frnd_arr = $this->Md_friend->extract_friend($user_id);
 		$data['friends'] = count($frnd_arr);
-		$this->load->view('profile',$data);
+		$this->load->view('dashboard',$data);
 	}
 }
 ?>
